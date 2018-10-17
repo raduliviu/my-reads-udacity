1b54454befcbb7fa9ad1ebf315be4f05084e96e4
Radu Baltag <baltag.radu@gmail.com>
2018-10-17 14:55:28 +0200

    fix: back arrow from search now works

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ const Search = () => {
   return (
     <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search"><Link to='/'>Close</Link></a>
+              <Link className="close-search" to='/'>Close</Link>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
